75806052c321963d8e4b9ba14510a58d688fe1f7
eins <eins@livingroom.fritz.box>
2017-04-29 19:40:26 +0200

    Processing Test

--- Processing01/src/de/hpi/processing/Story.java
+++ Processing01/src/de/hpi/processing/Story.java
@@ -2,9 +2,14 @@ package de.hpi.processing;
 
 import java.util.Arrays;
 
-public class Story {
+import processing.core.PApplet;
+
+public class Story extends PApplet {
+	
 	
 	public static void main (String[] args){
+		
+		
 		if(args.length > 0){
 			int i = 0;
 			for (String s : Arrays.asList(args)){
@@ -15,13 +20,31 @@ public class Story {
 		
 		System.out.println("Swing, AWT, WindowBuilder, Processing");
 		
+		PApplet.main("de.hpi.processing.Story");
+	}
+	
+	@Override
+    public void settings(){
+        size(800,800);
+    }
+			
+	@Override
+	public void setup(){
+		this.fill(100,30,200);
+	}
+	
+	@Override
+	public void draw(){
+		this.ellipse(100, 100, second(), second());
 		
 	}
-
+	
+    /*
 	public Story() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
+	*/
 	
 	
 
