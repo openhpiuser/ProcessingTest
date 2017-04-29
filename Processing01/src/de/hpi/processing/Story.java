6b2ed67f758e4be6978330d26804d4af815db138
eins <eins@livingroom.fritz.box>
2017-04-29 12:36:53 +0200

    Initial Commit

---
+++ Processing01/src/de/hpi/processing/Story.java
@@ -0,0 +1,16 @@
+package de.hpi.processing;
+
+public class Story {
+	
+	public static void main (String[] args){
+		System.out.println("test");
+	}
+
+	public Story() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+}
